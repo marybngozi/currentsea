32a3ab819bfabe578a93eb56ef72bfde7810d8b3
Mary Blessing <maryblessing.umeh@outlook.com>
2018-06-27 05:06:54 +0100

    adding javascript 1

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ fetch("https://free.currencyconverterapi.com/api/v5/currencies")
 .then((response) => {
   return response.json();
 })
-.then(data => {console.log(data)})
+.then(data => {console.log(data)});
